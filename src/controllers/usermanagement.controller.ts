8a6193b8a3d6ed28005b13b21b02e3dd1f3adfb3
Reza Aulia <zenhuwmank@gmail.com>
2019-02-20 04:45:46 +0700

    password bug

--- src/controllers/usermanagement.controller.ts
+++ src/controllers/usermanagement.controller.ts
@@ -150,8 +150,16 @@ export class UsermanagementController {
     @requestBody() user: User,
     @param.query.object('where', getWhereSchemaFor(User)) where?: Where,
   ): Promise<Count> {
-    console.log(moment(user.date_of_birth).format('DDMMYYYY'));
-    user.password = SHA256(moment(user.date_of_birth).format('DDMMYYYY'))
+    console.log(
+      moment(user.date_of_birth)
+        .utc(true)
+        .format('DDMMYYYY'),
+    );
+    user.password = SHA256(
+      moment(user.date_of_birth)
+        .utc(true)
+        .format('DDMMYYYY'),
+    )
       .toString()
       .toUpperCase();
     return await this.userRepository.updateAll(user, where);
